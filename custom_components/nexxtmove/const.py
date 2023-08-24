8a5b1487186b4b6534e739cf792e2e71980ff219
Geert Meersman <geertmeersman+github@gmail.com>
2023-08-24 11:27:34 +0200

    fix: f-string conversion

--- custom_components/nexxtmove/const.py
+++ custom_components/nexxtmove/const.py
@@ -45,14 +45,12 @@ DOMAIN = manifest_data.get("domain")
 NAME = manifest_data.get("name")
 VERSION = manifest_data.get("version")
 ISSUEURL = manifest_data.get("issue_tracker")
-STARTUP = """
+STARTUP = f"""
 -------------------------------------------------------------------
-{name}
-Version: {version}
+{NAME}
+Version: {VERSION}
 This is a custom component
 If you have any issues with this you need to open an issue here:
-{issueurl}
+{ISSUEURL}
 -------------------------------------------------------------------
-""".format(
-    name=NAME, version=VERSION, issueurl=ISSUEURL
-)
+"""
